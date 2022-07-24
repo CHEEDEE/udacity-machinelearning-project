9c737f62e23ea68042a48f45e00c864864407292
Chidiebere Alichi <chidiebere.alichi@9psb.com.ng>
2022-07-24 16:21:35 +0100

    upload to docker

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="swaydevstan/udacity-housing-app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-housing-app --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-housing-app --address 0.0.0.0 8000:80
